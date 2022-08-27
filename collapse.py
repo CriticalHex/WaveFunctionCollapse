c9340469eb8a3a8112872d59799acddd74e9e955
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2022-08-27 00:08:31 -0600

    half working

--- collapse.py
+++ collapse.py
@@ -13,6 +13,7 @@ class Tile:
     def __init__(self, fileName: str, sideValues: list):
         self.image = pygame.image.load("Tiles/"+fileName)
         self.sides = sideValues
+        self.name = fileName[0:5]
 
 TileImages = [Tile("Tile0.png", [0,0,0,0]), 
             Tile("Tile1.png", [1,1,0,1]),
@@ -24,57 +25,76 @@ class Space:
     def __init__(self,xpos,ypos):
         self.collapsed = False
         self.possibilities = TileImages.copy()
-        self.tile = self.possibilities[0]
         self.pos = (xpos,ypos)
         self.entropy = 5
+        self.tile: Tile
 
     def remove_possibility(self,slot):
         self.possibilities.remove(slot)
-        print(len(TileImages))
 
     def draw(self):
         if self.collapsed:
             screen.blit(self.tile.image, self.pos)
 
     def collapse(self):
-        self.collapsed = True
-        self.tile = self.possibilities[random.randint(0,len(self.possibilities)-1)]
+        if len(self.possibilities) != 0:
+            self.collapsed = True
+            self.tile = self.possibilities[random.randint(0,len(self.possibilities)-1)]
 
 def determine_possibilities():
-        for i,rows in enumerate(grid):
-            for j,tile in enumerate(rows):
-                if not tile.collapsed:
+    isDone = True
+    for i,rows in enumerate(grid):
+        for j,tile in enumerate(rows):
+            if not tile.collapsed:
+                isDone = False
+                if j+1 < len(grid[i]):
                     if grid[i][j+1].collapsed:
                         for possibility in tile.possibilities:
                             if grid[i][j+1].tile.sides[3] is not possibility.sides[1]:
                                 tile.remove_possibility(possibility)
+                if j-1 >= 0:
                     if grid[i][j-1].collapsed:
                         for possibility in tile.possibilities:
                             if grid[i][j-1].tile.sides[1] is not possibility.sides[3]:
                                 tile.remove_possibility(possibility)
+                if i+1 < len(grid):
                     if grid[i+1][j].collapsed:
                         for possibility in tile.possibilities:
                             if grid[i+1][j].tile.sides[0] is not possibility.sides[2]:
                                 tile.remove_possibility(possibility)
+                if i-1 >= 0:
                     if grid[i-1][j].collapsed:
                         for possibility in tile.possibilities:
                             if grid[i-1][j].tile.sides[2] is not possibility.sides[0]:
                                 tile.remove_possibility(possibility)
-                tile.entropy = len(tile.possibilities)
+            tile.entropy = len(tile.possibilities)
+    return isDone
 
 def collapse():
-    lowest = []
+    lowestEntropy = 5
+    lowestList = []
     for i,rows in enumerate(grid):
             for j,tile in enumerate(rows):
-                pass
-grid = []
-for i in range(10):
-    grid.append([])
-    for j in range(10):
-        grid[i].append(Space(j*100,i*100))
-        #grid[i][j].collapse()
+                if tile.entropy < lowestEntropy:
+                    lowestEntropy = tile.entropy
+                    print(lowestEntropy,end=", ")
+    for i,rows in enumerate(grid):
+            for j,tile in enumerate(rows):
+                if tile.entropy == lowestEntropy:
+                    lowestList.append(tile)
+    if len(lowestList) != 0:
+        print(len(lowestList))
+        lowestList[random.randint(0,len(lowestList)-1)].collapse()
+        return
+    print("NO LOWEST ENTROPY??")
 
-determine_possibilities(grid,5,5)
+done = False
+
+grid = []
+for i in range(int(screen.get_height()/100)):
+    grid.append([])
+    for j in range(int(screen.get_width()/100)):
+        grid[i].append(Space(j*100,i*100))
 
 while Running:
     for event in pygame.event.get():
@@ -83,8 +103,12 @@ while Running:
 
     screen.fill((0,0,0))
     
-    for i in range(10):
-        for j in range(10):
+    if not done:
+        done = determine_possibilities()
+        collapse()
+
+    for i in range(int(screen.get_height()/100)):
+        for j in range(int(screen.get_width()/100)):
             grid[i][j].draw()
 
     pygame.display.flip()
